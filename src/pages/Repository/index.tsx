7701751e3339fefbb598f94ec2b63524f6805ade
Alison Santos <alisonsantoso22@gmail.com>
2022-01-08 00:28:13 -0300

    Stiled repository info page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,13 +1,65 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+import logoImg from '../../assets/logo.svg';
 
 const Repository: React.FunctionComponent = () => {
   const { user, repository } = useParams();
 
   return (
-    <h1>
-      Repository: {user}/{repository}
-    </h1>
+    <>
+      <Header>
+        <img src={logoImg} width={400} alt="GitHub RepoExplorer" />
+
+        <Link to="/">
+          <FiChevronLeft size={18} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars.githubusercontent.com/u/84480176?v=4"
+            alt="Alison Santos"
+          />
+
+          <div>
+            <strong>alisonsantosofc/nodejs-users-api</strong>
+            <p>descrição do repositório.</p>
+          </div>
+        </header>
+
+        <ul>
+          <li>
+            <strong>850</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="sdsdf">
+          <div>
+            <strong>alisonsantosofc/nodejs-users-api</strong>
+            <p>Uma API para usuários.</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
   );
 };
 
---
+++ src/pages/Repository/styles.ts
@@ -0,0 +1,124 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  a {
+    display: flex;
+    align-items: center;
+    font-size: 22px;
+    text-decoration: none;
+    color: #a8a8b3;
+    transition: color 0.2s;
+
+    &:hover {
+      color: #666;
+    }
+
+    svg {
+      margin-right: 4px;
+    }
+  }
+`;
+
+export const RepositoryInfo = styled.section`
+  margin-top: 70px;
+
+  header {
+    display: flex;
+    align-items: center;
+
+    img {
+      width: 120px;
+      height: 120px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 24px;
+
+      strong {
+        font-size: 36px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #737380;
+        margin-top: 4px;
+      }
+    }
+  }
+
+  ul {
+    display: flex;
+    list-style: none;
+    margin-top: 40px;
+
+    li {
+      text-align: center;
+
+      & + li {
+        margin-left: 50px;
+      }
+
+      strong {
+        display: block;
+        font-size: 36px;
+        color: #3d3d4d;
+      }
+
+      span {
+        display: block;
+        margin-top: 4px;
+        color: #6c6c80;
+      }
+    }
+  }
+`;
+
+export const Issues = styled.section`
+  margin-top: 40px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    & + a {
+      margin-top: 10px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    div {
+      flex: 1;
+      margin: 0 16px;
+
+      strong {
+        font-size: 18px;
+        color: #3d3d4d;
+      }
+
+      p {
+        margin-top: 4px;
+        font-size: 16px;
+        color: #a8a8b3;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
+`;
