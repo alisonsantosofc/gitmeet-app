6896e38983e6e0dac97e6058a355ba356149cb5e
Alison Santos <alisonsantoso22@gmail.com>
2022-01-08 10:58:57 -0300

    Looking for results in the api

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
+import api from '../../services/api';
 import { Header, RepositoryInfo, Issues } from './styles';
 
 import logoImg from '../../assets/logo.svg';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  open_issues_count: number;
+  forks_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+interface Issue {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  };
+}
+
 const Repository: React.FunctionComponent = () => {
+  const [repositoryData, setRepositoryData] = useState<Repository | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
+
   const { user, repository } = useParams();
+
+  useEffect(() => {
+    api.get(`repos/${user}/${repository}`).then((response) => {
+      setRepositoryData(response.data);
+    });
+
+    api.get(`repos/${user}/${repository}/issues`).then((response) => {
+      setIssues(response.data);
+    });
+  }, [repository]); // eslint-disable-line
 
   return (
     <>
@@ -20,44 +55,48 @@ const Repository: React.FunctionComponent = () => {
         </Link>
       </Header>
 
-      <RepositoryInfo>
-        <header>
-          <img
-            src="https://avatars.githubusercontent.com/u/84480176?v=4"
-            alt="Alison Santos"
-          />
+      {repositoryData && (
+        <RepositoryInfo>
+          <header>
+            <img
+              src={repositoryData.owner.avatar_url}
+              alt={repositoryData.owner.login}
+            />
 
-          <div>
-            <strong>alisonsantosofc/nodejs-users-api</strong>
-            <p>descrição do repositório.</p>
-          </div>
-        </header>
+            <div>
+              <strong>{repositoryData.full_name}</strong>
+              <p>{repositoryData.description}</p>
+            </div>
+          </header>
 
-        <ul>
-          <li>
-            <strong>850</strong>
-            <span>Stars</span>
-          </li>
-          <li>
-            <strong>48</strong>
-            <span>Forks</span>
-          </li>
-          <li>
-            <strong>67</strong>
-            <span>Issues</span>
-          </li>
-        </ul>
-      </RepositoryInfo>
+          <ul>
+            <li>
+              <strong>{repositoryData.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{repositoryData.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repositoryData.open_issues_count}</strong>
+              <span>Issues</span>
+            </li>
+          </ul>
+        </RepositoryInfo>
+      )}
 
       <Issues>
-        <Link to="sdsdf">
-          <div>
-            <strong>alisonsantosofc/nodejs-users-api</strong>
-            <p>Uma API para usuários.</p>
-          </div>
+        {issues.map((issue) => (
+          <a key={issue.id} href={issue.html_url}>
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </Link>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Issues>
     </>
   );
