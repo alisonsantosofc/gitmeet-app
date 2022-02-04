e4015993f8578fbb5f7106ce74d5669036c03585
Alison Santos <alisonsantoso22@gmail.com>
2022-02-04 16:55:08 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,7 @@ import Repository from '../pages/Repository';
 const MainRoutes: React.FunctionComponent = () => {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />;
+      <Route path="/" element={<Dashboard />} />
       <Route path="/repositories/" element={<Repository />}>
         <Route path=":user">
           <Route path=":repository" />
